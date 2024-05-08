eab7c1acb4390415c18eb853c11c578e451981a2
Ilya <ii002120@g.bstu.by>
2024-05-08 16:36:38 +0300

    Editing __init__.py

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -20,7 +20,8 @@ file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
 app = Flask(__name__)
-app.secret_key = 'some secret salt'
+app.secret_key = 'some secret salting'
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ilya:test@localhost/test_bd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
